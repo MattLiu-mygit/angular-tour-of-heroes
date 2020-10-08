3d3fc1976417b8ee812165ad118488fddf2cbe8b
MattLiu-mygit <mliu78@jh.edu>
2020-10-07 22:42:10 -0400

    Implemented more services that could be used throughout the app to produce asynchronous or component-wide reusable logic(look more into services), learned routing using app-rounting module

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,20 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
   heroes: Hero[];
-  constructor(private heroService: HeroService) {}
+
+  selectedHero: Hero;
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
